be93a9cb596cf761a42a39299b76d2bddcf7c9a7
Rony João de Sousa <ronyjdesousa@gmail.com>
2026-05-22 08:02:23 -0300

    Modernize banner output and simplify project metadata.

--- src/arguments.cpp
+++ src/arguments.cpp
@@ -1,112 +1,167 @@
 #include "standard.h"
 
-
 using namespace std;
-
 
 void arguments_c::PrintBanner()
 {
-	cout << "paping v" << VERSION << " - Copyright (c) " << YEAR << " " << AUTHOR << endl << endl;
+        cout << "paping " << VERSION << endl;
+        cout << "Originally written by " << AUTHOR << endl;
+        cout << "Maintained by the " << MAINTAINER << endl;
+        cout << endl;
 }
-
 
 void arguments_c::PrintUsage()
 {
-	cout << i18n_c::GetString(STRING_USAGE) << endl;
+        cout << i18n_c::GetString(STRING_USAGE) << endl;
 }
 
-
-int	arguments_c::Process(int argc, pc_t argv[], arguments_c &arguments)
+int arguments_c::Process(int argc, pc_t argv[], arguments_c &arguments)
 {
-	int		result			= 0;
-	int		value			= 0;
-	bool	anyMatch		= false;
-	bool	gotDestination	= false;
+        int result = 0;
+        int value = 0;
 
-	arguments.Port		= 0;
-	arguments.Count		= 0;
-	arguments.Timeout	= 1000;
-	arguments.Type		= IPPROTO_TCP;
-	arguments.Continous	= true;
-	arguments.UseColor	= true;
+        bool anyMatch = false;
+        bool gotDestination = false;
 
-	for (int i=1; i<argc; i++)
-	{
-		anyMatch	= false;
+        arguments.Port = 0;
+        arguments.Count = 0;
+        arguments.Timeout = 1000;
+        arguments.Type = IPPROTO_TCP;
+        arguments.Continous = true;
+        arguments.UseColor = true;
 
-		if (result = arguments_c::match(i, argc, argv, "-p", "--port", true, arguments.Port, anyMatch) != SUCCESS) return result;
-		if (result = arguments_c::match(i, argc, argv, "-c", "--count", true, arguments.Count, anyMatch) != SUCCESS) return result;
-		if (result = arguments_c::match(i, argc, argv, "-t", "--timeout", true, arguments.Timeout, anyMatch) != SUCCESS) return result;
+        for (int i = 1; i < argc; i++)
+        {
+                anyMatch = false;
 
-		if (anyMatch)
-		{
-			i++;
-			continue;
-		}
+                if ((result = arguments_c::match(i, argc, argv,
+                        "-p", "--port", true,
+                        arguments.Port, anyMatch)) != SUCCESS)
+                {
+                        return result;
+                }
 
-		if (result = arguments_c::match(i, argc, argv, NULL, "--nocolor", false, value, anyMatch) != SUCCESS) return result;
-		if (value == 1) arguments.UseColor = false;
+                if ((result = arguments_c::match(i, argc, argv,
+                        "-c", "--count", true,
+                        arguments.Count, anyMatch)) != SUCCESS)
+                {
+                        return result;
+                }
 
+                if ((result = arguments_c::match(i, argc, argv,
+                        "-t", "--timeout", true,
+                        arguments.Timeout, anyMatch)) != SUCCESS)
+                {
+                        return result;
+                }
 
-		if (!anyMatch)
-		{
-			if (!gotDestination)
-			{
-				gotDestination			= true;
-				arguments.Destination	= argv[i];
-			}
-			else
-			{
-				return ERROR_INVALIDARGUMENTS;
-			}
-		}
-	}
+                if (anyMatch)
+                {
+                        i++;
+                        continue;
+                }
 
-	if (arguments.Count <= 0)
-	{
-		arguments.Count		= -1;
-		arguments.Continous = true;
-	}
-	else
-	{
-		arguments.Continous = false;
-	}
+                if ((result = arguments_c::match(i, argc, argv,
+                        NULL, "--nocolor", false,
+                        value, anyMatch)) != SUCCESS)
+                {
+                        return result;
+                }
 
-	if (arguments.Port <= 0) return ERROR_INVALIDARGUMENTS;
-	if (arguments.Timeout <= 0) return ERROR_INVALIDARGUMENTS;
-	if (!gotDestination) return ERROR_INVALIDARGUMENTS;
+                if (value == 1)
+                {
+                        arguments.UseColor = false;
+                }
 
-	return SUCCESS;
+                if (!anyMatch)
+                {
+                        if (!gotDestination)
+                        {
+                                gotDestination = true;
+                                arguments.Destination = argv[i];
+                        }
+                        else
+                        {
+                                return ERROR_INVALIDARGUMENTS;
+                        }
+                }
+        }
+
+        if (arguments.Count <= 0)
+        {
+                arguments.Count = -1;
+                arguments.Continous = true;
+        }
+        else
+        {
+                arguments.Continous = false;
+        }
+
+        if (arguments.Port <= 0)
+        {
+                return ERROR_INVALIDARGUMENTS;
+        }
+
+        if (arguments.Timeout <= 0)
+        {
+                return ERROR_INVALIDARGUMENTS;
+        }
+
+        if (!gotDestination)
+        {
+                return ERROR_INVALIDARGUMENTS;
+        }
+
+        return SUCCESS;
 }
 
-
-int	arguments_c::match(int pos, int total, pc_t argv[], pcc_t shortName, pcc_t longName, bool expectedValue, int &value, bool &anyMatch)
+int arguments_c::match(
+        int pos,
+        int total,
+        pc_t argv[],
+        pcc_t shortName,
+        pcc_t longName,
+        bool expectedValue,
+        int &value,
+        bool &anyMatch)
 {
-	if	(
-				(	shortName != NULL						&&
-					strlen(shortName) == strlen(argv[pos])	&&
-					!strcmp(shortName, argv[pos]))
-			||	(	longName != NULL						&&
-					strlen(longName) == strlen(argv[pos])	&&
-					!strcmp(longName, argv[pos]))
-		)
-		{
-			anyMatch = true;
+        if (
+                (
+                        shortName != NULL &&
+                        strlen(shortName) == strlen(argv[pos]) &&
+                        !strcmp(shortName, argv[pos])
+                )
+                ||
+                (
+                        longName != NULL &&
+                        strlen(longName) == strlen(argv[pos]) &&
+                        !strcmp(longName, argv[pos])
+                )
+        )
+        {
+                anyMatch = true;
 
-			if (expectedValue)
-			{
-				if (pos+1 == total) return ERROR_INVALIDARGUMENTS;
-				value	= atoi(argv[pos+1]);
-			}
-			else
-			{
-				value = 1;
-			}
+                if (expectedValue)
+                {
+                        if (pos + 1 == total)
+                        {
+                                return ERROR_INVALIDARGUMENTS;
+                        }
 
-			return SUCCESS;
-		}
+                        value = atoi(argv[pos + 1]);
+                }
+                else
+                {
+                        value = 1;
+                }
 
-	if (!expectedValue) value = 0;
+                return SUCCESS;
+        }
 
-	return SUCCESS;
+        if (!expectedValue)
+        {
+                value = 0;
+        }
+
+        return SUCCESS;
 }
--- src/standard.h
+++ src/standard.h
@@ -1,28 +1,28 @@
 #pragma once
 
 #ifdef WIN32
-	#define WIN32_LEAN_AND_MEAN
-	#define _CRT_SECURE_NO_WARNINGS 1
-	#pragma comment(lib, "ws2_32.lib")
+        #define WIN32_LEAN_AND_MEAN
+        #define _CRT_SECURE_NO_WARNINGS 1
+        #pragma comment(lib, "ws2_32.lib")
 #endif
 
-#define	VERSION	"1.5.5"
-#define	AUTHOR	"Mike Lovell"
-#define YEAR	((((__DATE__ [7] - '0') * 10 + (__DATE__ [8] - '0')) * 10 + (__DATE__ [9] - '0')) * 10 + (__DATE__ [10] - '0'))
+#define VERSION           "1.6.0"
+#define AUTHOR            "Mike Lovell"
+#define MAINTAINER        "paping contributors"
+#define COPYRIGHT_YEARS   "2004-2026"
 
 #ifdef WIN32
-	#define close	closesocket
-	#define snprintf _snprintf
+        #define close     closesocket
+        #define snprintf  _snprintf
 #endif
 
 // Error codes
-#define	SUCCESS						0
-#define	ERROR_POUTOFMEMORY			100
-#define	ERROR_SOCKET_CANNOTRESOLVE	101
-#define	ERROR_SOCKET_TIMEOUT		102
-#define	ERROR_SOCKET_GENERALFAILURE	103
-#define	ERROR_INVALIDARGUMENTS		200
-
+#define SUCCESS                         0
+#define ERROR_POUTOFMEMORY             100
+#define ERROR_SOCKET_CANNOTRESOLVE     101
+#define ERROR_SOCKET_TIMEOUT           102
+#define ERROR_SOCKET_GENERALFAILURE    103
+#define ERROR_INVALIDARGUMENTS         200
 
 #include <iostream>
 #include <string.h>
@@ -30,18 +30,20 @@
 #include <signal.h>
 #include <stdio.h>
 
-#ifdef WIN32	// Windows specific
-	#include <Windows.h>
-	#include <WinSock2.h>
-	#include <time.h>
-	#include "gettimeofday.h"
-#else			// Linux specific
+#ifdef WIN32    // Windows specific
 
-	#include <sys/socket.h>
-	#include <arpa/inet.h>
-	#include <netdb.h>
-	#include <unistd.h>
-	#include <sys/time.h>
+        #include <Windows.h>
+        #include <WinSock2.h>
+        #include <time.h>
+        #include "gettimeofday.h"
+
+#else           // POSIX specific
+
+        #include <sys/socket.h>
+        #include <arpa/inet.h>
+        #include <netdb.h>
+        #include <unistd.h>
+        #include <sys/time.h>
 
 #endif
 
@@ -49,11 +51,11 @@
 #include <fcntl.h>
 
 // Types
-typedef	const		wchar_t*	pcw_t;
-typedef	const		char*		pcc_t;
-typedef				wchar_t*	pwc_t;
-typedef				char*		pc_t;
-typedef	unsigned	short		ushort_t;
+typedef const wchar_t*   pcw_t;
+typedef const char*      pcc_t;
+typedef wchar_t*         pwc_t;
+typedef char*            pc_t;
+typedef unsigned short   ushort_t;
 
 #include "print.h"
 #include "i18n.h"
@@ -62,5 +64,3 @@ typedef	unsigned	short		ushort_t;
 #include "arguments.h"
 #include "host.h"
 #include "socket.h"
-
-
